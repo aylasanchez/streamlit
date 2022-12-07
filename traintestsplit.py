c149fe27d510c56a32d561a23027c33a49c1615f
aylasanchez <116188217+aylasanchez@users.noreply.github.com>
2022-12-07 04:36:20 -0500

    Update traintestsplit.py

--- traintestsplit.py
+++ traintestsplit.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import r_regression
 
